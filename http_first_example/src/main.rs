c9a71f98535dbbfd5c23717352bd2e841d199909
Adrian González Pardo <gozapaadr@gmail.com>
2023-05-03 10:05:41 -0600

    fix: refactor server main code

--- http_first_example/src/main.rs
+++ http_first_example/src/main.rs
@@ -10,23 +10,6 @@ fn main() {
      * IP 0.0.0.0 wildcard for listen in every netiface in port 8080
      * You can view your implementation when run with lsof -i:8080 (in Linux rookie)
      **/
-    // let get = Method::GET;
-    // let delete = Method::DELETE;
-    // let post = Method::POST;
-    // let put = Method::PUT;
-    // let head = Method::HEAD;
-    // let connect = Method::CONNECT;
-    // let options = Method::OPTIONS;
-    // let trace = Method::TRACE;
-    // let patch = Method::PATCH;
-    let server_host = String::from("0.0.0.0:8080");
-    // let server_slice = &server_host[..4];
-    // let server_borrow: &str = &server_host; // <- immutable variable
-    // let string_literal = "12345"; // <- immutable variable
-    // dbg!(&server_host);
-    // dbg!(server_slice);
-    // dbg!(server_borrow);
-    // dbg!(string_literal);
-    let server = Server::new(server_host);
+    let server = Server::new("0.0.0.0:8080".to_string());
     server.run();
 }
